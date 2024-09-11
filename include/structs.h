10db0b676814f4d70b01bf34202bb8f1ab28020f
Naib Adam <naadam@lab1r4s1.42abudhabi.ae>
2024-09-11 22:35:33 +0400

    one file

--- include/structs.h
+++ include/structs.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naadam <naadam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 22:27:34 by naadam            #+#    #+#             */
+/*   Updated: 2024/09/11 22:32:42 by naadam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
@@ -11,21 +23,21 @@ typedef struct s_map
 	int		height;
 }			t_map;
 
-typedef struct	s_cur
+typedef struct s_cur
 {
-	int x;
-	int y;
-} 			t_cur;
+	int	x;
+	int	y;
+}		t_cur;
 
 typedef struct s_window
 {
 	void	*mlx;
 	void	*window;
-	void 	*img;
-    char	*addr;
-    int		bits_per_pixel;
-    int		line_len;
-    int		endian;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_len;
+	int		endian;
 	int		width;
 	int		height;
 	int		color;
@@ -62,61 +74,59 @@ typedef struct s_window
 	int		texx;
 	int		texy;
 	double	texxx;
-}              t_window;
+}		t_window;
 
-typedef	struct s_texture
+typedef struct s_texture
 {
-	char *no;
-	char *so;
-	char *we;
-	char *ea;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
 }			t_texture;
 
 typedef struct s_color
 {
 	int	*r;
-	int *g;
-	int *b;
-	int	cflag; // 0 means floor color is first else start with ceiling
-	int floor_color;
+	int	*g;
+	int	*b;
+	int	cflag;
+	int	floor_color;
 	int	ceiling_color;
 }			t_color;
 
 typedef struct s_point
 {
-	char	*row;
-	int		x_length;
-	struct	s_point *next;
+	char			*row;
+	int				x_length;
+	struct s_point	*next;
 }			t_point;
 
-typedef	struct s_player
+typedef struct s_player
 {
-	double pos_x;
-	double pos_y;
-	double dir_x;
-	double dir_y;
-	double angle;
-	double pixel_width;
-	double pixel_height;
-	double plane_length;
-	double plane_x;
-	double plane_y;
-	double camerax;
-	double raydir_x;
-	double raydir_y;
-	double sidedistx;
-	double sidedisty;
-	double perpwalldist;
-	int	   stepx;
-	int	   stepy;
-	int	   side;
-	double deltadistx;
-	double deltadisty;
-	double endRayY;
-	double endRayX;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	angle;
+	double	pixel_width;
+	double	pixel_height;
+	double	plane_length;
+	double	plane_x;
+	double	plane_y;
+	double	camerax;
+	double	raydir_x;
+	double	raydir_y;
+	double	sidedistx;
+	double	sidedisty;
+	double	perpwalldist;
+	int		stepx;
+	int		stepy;
+	int		side;
+	double	deltadistx;
+	double	deltadisty;
 }		t_player;
 
-typedef	struct s_parse
+typedef struct s_parse
 {
 	char		*file_path;
 	char		*buf;
@@ -125,16 +135,16 @@ typedef	struct s_parse
 	int			encountered;
 	int			max;
 	int			rows;
-	t_texture 	*texture;
+	t_texture	*texture;
 	t_color		*color;
 	t_point		*point;
 }				t_parse;
 
 typedef struct s_wall
 {
-	int	line_height;
-	int	draw_start;
-	int	draw_end;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
 	double	wallx;
 }			t_wall;
 
